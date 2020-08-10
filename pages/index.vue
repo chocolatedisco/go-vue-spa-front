0189158b516c1f8148dfe7910d931556c7ceaa37
chocolatedisco <yuichiro.hack@gmail.com>
2020-08-11 00:14:47 +0900

    init

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <section>
+      </br>
+      <div class="container">
+        <b-field>
+            <b-input placeholder="Email" type="email"></b-input>
+        </b-field>
+        <b-field>
+            <b-input type="password"
+                placeholder="Regular password input">
+            </b-input>
+        </b-field>
+        </div>
+    </section>
+</template>
