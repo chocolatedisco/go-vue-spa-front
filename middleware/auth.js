256c91c6a44595e029df348bdb090032137a7784
chocolatedisco <yuichiro.hack@gmail.com>
2020-08-13 01:14:16 +0900

    keep login but routing error

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,7 @@
 import firebase from '~/plugins/firebase'
 
-export default function ({ route, store, redirect }) {
-firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-    store.dispatch("auth/gotUser", user)
-    } else {
-    if(route.fullPath !== '/login') redirect("/login")
+export default function ({ store, route, redirect }) {
+    if (!store.getters.isAuthenticated && route.fullPath !== '/login') {
+      redirect('/login')
     }
-})
-}
-
+  }
--- pages/login.vue
+++ pages/login.vue
@@ -12,6 +12,8 @@
 
 <script>
 import firebase from '~/plugins/firebase'
+import { mapActions } from 'vuex'
+
 export default {
   name: 'Signin',
   data: function () {
@@ -21,16 +23,29 @@ export default {
     }
   },
   methods: {
+    ...mapActions([
+      'setUser'
+    ]),
     signIn: function () {
       firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(res => {
         localStorage.setItem('jwt', res.user.qa)
-        this.$router.push('/mypage')
+        this.$router.push('/login')
       }, err => {
         alert(err.message)
       })
     }
+  },
+  async mounted () {
+    let user = await new Promise((resolve, reject) => {
+      firebase.auth().onAuthStateChanged((user) => resolve(user))
+    })
+    this.setUser(user) // setUser is mapped action from vuex
+    if (user) {
+      this.$router.push('/mypage') // if non-null user given, go to my page.
+    }
   }
 }
+
 </script>
 
 
